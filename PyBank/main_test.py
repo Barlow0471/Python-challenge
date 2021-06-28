5320c58fa943d39e54b2adf5f364c25cf2d07064
Barlow0471 <mattbar2@comcast.net>
2021-06-28 16:18:54 -0400

    Messing around trying to find average change

--- PyBank/main_test.py
+++ PyBank/main_test.py
@@ -13,27 +13,31 @@ with open(csvpath) as csvfile:
     # Skip header
     csv_reader = next(csvreader, None)
 
+    # Create empty lists
     dates = []
     profit_losses = []
 
+    # Create loop for separate lists
     for row in csvreader:
         money = row[1]
         date = row[0]
 
         dates.append(date)
         profit_losses.append(money)
-    'Determine total number of months'
+
+    # Determine total number of months
     total_months = 0
     for month in dates:
         total_months += 1
 
-    'Determine net total amount'
+    # Determine net total amount
     net_total = 0
     for row in profit_losses:
         net_total += int(row)
 
-print(statistics.mean(profit_losses))
-print(net_total)
-print(total_months)
+print(max(profit_losses))
+print("Financial Analysis\n")
+print("Total Months: " + str(total_months))
+print("Total: $" + str(net_total))
 # print(dates)
 # print(profit_losses)
