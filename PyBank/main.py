940bd1a23332ee0e9684133942bcb07fdb3002fc
Barlow0471 <mattbar2@comcast.net>
2021-06-28 11:12:27 -0400

    Added month counter

---
+++ PyBank/main.py
@@ -0,0 +1,26 @@
+# Modules
+import os
+import csv
+
+# Set path for file
+csvpath = os.path.join('PyBank','Resources','budget_data.csv')
+
+# Open the CSV
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Skip header
+    csv_reader = next(csvreader)
+
+    # Set up loop for months
+    month_counter = 0
+    total_counter = 0
+    for row in csvreader:
+        month_counter += 1
+    
+    print("Total Months: " + str(month_counter))
+    # print(total_counter)
+    # total_counter = 0
+    # for i in csvreader:
+    #     total_counter += 1
+    # print(total_counter)
